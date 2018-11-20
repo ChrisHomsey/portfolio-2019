b36118069b078ebd473e87b1777290301ca89158
Chris <chomsey1@gmail.com>
2018-11-19 22:19:06 -0500

    added photos, portfolio section

--- public/js/index.js
+++ public/js/index.js
@@ -28,6 +28,7 @@ document.getElementById('nav-home').addEventListener('click', function() {
     document.getElementById('nav-portfolio').classList.remove('active');
     document.getElementById('nav-contact').classList.remove('active');
     document.getElementById('nav-home').classList.add('active');
+    document.getElementById('nav-bar').classList.remove('portfolio-active');
 
     document.getElementById('contact').classList.add('closed');
     document.getElementById('portfolio').classList.add('closed');
@@ -35,28 +36,28 @@ document.getElementById('nav-home').addEventListener('click', function() {
 
 });
 
+
 // if the user selects portfolio -> set it to active, open #portfolio and close the other divs
 document.getElementById('nav-portfolio').addEventListener('click', function() {
-    document.getElementById('nav-home').classList.remove('active');
-    document.getElementById('nav-contact').classList.remove('active');
-    document.getElementById('nav-portfolio').classList.add('active');
-
-    document.getElementById('content-min').classList.add('closed');
-    document.getElementById('contact').classList.add('closed');
-    document.getElementById('portfolio').classList.remove('closed');
-
+    openPortfolio();
 });
+
+// if the user selects the toggle-portfolio button in the bio, toggle the portfolio
+document.getElementById('toggle-portfolio-button').addEventListener('click', function(){
+    openPortfolio();
+})
 
 // if the user selects contact -> set it to active, open #contact and close the other divs
 document.getElementById('nav-contact').addEventListener('click', function() {
     document.getElementById('nav-home').classList.remove('active');
     document.getElementById('nav-portfolio').classList.remove('active');
     document.getElementById('nav-contact').classList.add('active');
+    document.getElementById('nav-bar').classList.remove('portfolio-active');
 
     document.getElementById('content-min').classList.add('closed');
+    document.getElementById('content-max').classList.add('closed');
     document.getElementById('portfolio').classList.add('closed');
     document.getElementById('contact').classList.remove('closed');
-
 });
 
 
@@ -79,6 +80,30 @@ document.getElementById('bio-close-button').addEventListener('click', function()
 })
 
 
+// Opens the portfolio section
+var openPortfolio = function() {
+    document.getElementById('nav-home').classList.remove('active');
+    document.getElementById('nav-contact').classList.remove('active');
+    document.getElementById('nav-portfolio').classList.add('active');
+    document.getElementById('nav-bar').classList.add('portfolio-active');
+
+    document.getElementById('content-min').classList.add('closed');
+    document.getElementById('content-max').classList.add('closed');
+    document.getElementById('contact').classList.add('closed');
+    document.getElementById('portfolio').classList.remove('closed');
+}
+
+// Toggle between dev and photography portfolios
+document.getElementById('toggle-dev').addEventListener('click', function(){
+    document.getElementById('portfolio-photography').classList.add('hidden');
+    document.getElementById('portfolio-dev').classList.remove('hidden');
+})
+document.getElementById('toggle-photography').addEventListener('click', function(){
+    document.getElementById('portfolio-dev').classList.add('hidden');
+    document.getElementById('portfolio-photography').classList.remove('hidden');
+})
+
+
 // Function called to open the large bio div
 var openMaxBio = function() {
     var minBio = document.getElementById('content-min'); 
@@ -97,4 +122,31 @@ var openMinBio = function() {
     maxBio.removeEventListener(transitionEnd, openMinBio);
 }
     
-    
+var renderPhotoGallery = function() {
+
+    var columnIndex = 0;
+    var photoColumns = [[],[],[],[]];
+
+    // Loop for every photo of the PhotoData array
+    for (var i = 0; i < 35; i++) {
+
+        //Declares the newImage element to be pushed
+        var urlTemplate = './img/photography/thumbnails/thumb-' + i + '.jpg';
+        var imageTemplate = '<img class="photo" src="'+urlTemplate+'"/>';
+        
+        // Targets the appropriate photoColumn subarray and pushes the newImage into it's array
+        photoColumns[columnIndex].push(imageTemplate);
+
+        // Recursively increments the columnIndex and resets to 0 if it reaches > 3
+        columnIndex++;
+        if (columnIndex > 3) columnIndex = 0;            
+    }
+
+    // Render each of the arrays of photos into their respective columns    
+    document.querySelector('#photo-column-one').innerHTML = photoColumns[0];
+    document.querySelector('#photo-column-two').innerHTML = photoColumns[1];
+    document.querySelector('#photo-column-three').innerHTML = photoColumns[2];
+    document.querySelector('#photo-column-four').innerHTML = photoColumns[3]; 
+}
+
+renderPhotoGallery();
